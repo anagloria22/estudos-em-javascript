97d2ca6ce00d818007d6c76a6977738d73a67565
Ana Glória <anaparente196@gmail.com>
2024-04-16 19:20:47 -0300

    create variáveis

---
+++ variáveis/script.js
@@ -0,0 +1,36 @@
+var nome = 'Ana'
+var idade = 20
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade, nome)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco)
+
+var sobrenome = 'Parente', cidade = 'Sobral'
+
+console.log(sobrenome, cidade)
+
+var semDefinir
+console.log(semDefinir)
+//console.log(aindaNaoDefinida)
+
+var comida
+console.log(comida)
+var comida = 'pizza'
+console.log(comida)
+
+var esporte = 'Futebol'
+var esporte = 'Formula 1' //com let não é possível fazer isso, só dá pra declarar let 1 vez
+console.log(esporte)
+
+var equipe = 'Mercedes'
+equipe = 'McLaren'
+console.log(equipe)
+
+const piloto = 'Verstappen'
+console.log(piloto) //é uma constante
+
---
+++ variáveis/script2.js
@@ -0,0 +1,10 @@
+var nome = 'Ana Gloria'
+var idade = 20
+var comida
+comida = 'salgadinho'
+var equipe = 'McLaren',
+    esporte,
+    piloto,
+    corrida,
+    circuito
+console.log(nome, idade, comida, equipe, corrida)
