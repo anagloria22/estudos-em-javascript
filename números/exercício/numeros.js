3eaf2c7ca79a75094540a4f491868aee0158eba9
Ana Glória <anaparente196@gmail.com>
2024-04-17 16:57:54 -0300

    update

---
+++ números/exercício/numeros.js
@@ -0,0 +1,20 @@
+var total = 10 + 5 * 2 / 2 + 20
+console.log(total)
+
+var dia = 'dia 18' / 2
+console.log(isNaN(dia))
+
+var n1 = +'200'
+var n2 = 50
+var soma = n1 + n2
+console.log(soma)
+
+var numero = 5
+console.log(++numero)
+
+var peso = 57
+var unidade = 'kg'
+var total = peso + unidade
+var pesoPorDois = peso / 2
+console.log(`Eu peso ${total} e meu peso dividido por 2 é ${pesoPorDois}`)
+console.log(isNaN(pesoPorDois))
--- números/numero.js
+++ números/numero.js
@@ -12,3 +12,21 @@ var divisao = 'Comprei 10' / 2 //NaN(not a number), porque tem caractere junto d
 
 var testNaN = 'isso é 100' / 2
 console.log(isNaN(testNaN)) // retorna true já que não é um número pois tem caractere na operação
+
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10 
+console.log(soma1)
+
+//Operadores Unários(incremento e decremento)
+var incremento = 5
+console.log(incremento++) //continua a mesma coisa, mas quando chamamos de novo
+console.log(incremento) //ele adiciona +1 ao valor que estava antes do ++, é o equivalente a "incremento = incremento + 1"
+// isso só épossível, porque é var(ou let), se for const não dá, porque é uma constante e não pode ser modificada
+
+// decremento é o contrário, trocamos ++ por -- e ele diminui -1
+var decremento = 10
+console.log(decremento--) //quando colocamos -- depois ele primeiro mostra o valor que era e depois o que passa a ser com --, quando colocamos -- antes ele mostra logo o valor declarado -1
+console.log(decremento)
+
+//o + na frente do valor de uma string de número o transforma em formato de número
+var idade = +'20'
+console.log(typeof idade)
