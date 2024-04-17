f43a95ed7050794cde3e5ef74bf6b17898c7857e
Ana Glória <anaparente196@gmail.com>
2024-04-17 19:06:02 -0300

    update

---
+++ números/boolean/boolean.js
@@ -0,0 +1,8 @@
+var possuiGraduacao = false
+var possuiDoutorado = true
+
+if(possuiGraduacao) {
+    console.log('Possui graduação')
+} else if (possuiDoutorado) {
+    console.log('Não possui graduação, possui é Doutorado. Chame de dotô.')
+}
