ab344ba5b0fde930b259ca3ae654293b291526bd
Ana Glória <anaparente196@gmail.com>
2024-04-16 21:08:44 -0300

    update

---
+++ tipos de dados/exercicio/dados2.js
@@ -0,0 +1,24 @@
+var nome = 'Ana Glória'
+console.log(typeof nome)
+
+var verificarTipoNome = typeof nome
+console.log(verificarTipoNome)
+
+var idade = '20' //tipo string
+console.log(typeof idade)
+
+var idade2 = 20 //tipo number
+console.log(typeof idade2)
+
+var nomes = 'Ana Glória'
+var sobrenome = 'de Oliveira Parente'
+/*var nomeCompleto = `${nome} ${sobrenome}`
+  var nomeCompleto = nomes + ' ' + sobrenome
+  os resultados também serão: Ana Glória de Oliveira Parente para ambas as maneiras*/
+
+console.log(`${nome} ${sobrenome}`)
+
+var frase = "It's time"
+console.log(frase)
+
+
