ec7e2db671a295441a1fa75bafe4882f69c1c4c8
Ana Glória <anaparente196@gmail.com>
2024-04-20 01:18:15 -0300

    update

--- números/boolean/boolean.js
+++ números/boolean/boolean.js
@@ -6,3 +6,18 @@ if(possuiGraduacao) {
 } else if (possuiDoutorado) {
     console.log('Não possui graduação, possui é Doutorado. Chame de dotô.')
 }
+
+var x = 10
+console.log(x !== 11)
+
+if((5 - 5) && (5 + 5)) { //por 5-5 ser 0 e 0 ser um valor falso ele já retorna falso
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+}
+
+if((5 - 10) && (5 + 5)) { //por 5-10 ser um número negativo, mesmo que seja negativo é diferente de 0, então ele considera verdadeiro
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+}
