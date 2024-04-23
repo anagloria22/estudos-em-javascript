73d6e1fcff5d38be1e48bfbedbdfe7345b82c192
Ana Glória <anaparente196@gmail.com>
2024-04-23 16:28:50 -0300

    Update funcao.js

--- funções/funcao.js
+++ funções/funcao.js
@@ -35,9 +35,43 @@ console.log(corFavorita('verde'))
 /* Argumentos podem ser funções, quando isso ocorre chamamos de callback.
 Geralmente ocorrem depois de um evento*/
 
-addEventListener('click', function() {
+function mostraConsole() {
     console.log('Clicou')
-})
+}
+
+addEventListener('click', mostraConsole) //exibe a quantidade de vezes clicadas na tela
+
 //essa função tem 2 argumentos
 //primeiro é a string 'click'
 //segundo é uma função anônima
+
+function imc2(peso, altura) {
+    const imc2 = peso / (altura ** 2)
+    console.log(imc2)
+}
+
+imc2(80, 1.80) //undefined
+//se escrevermos no console do navegador o imc2(valor1, valor2) ele vai retornar o resultado do cálculo e undefined, porque não foi definido o return
+
+//uma funcção pode retornar qualquer tipo de dado ou outras funções, mas não é uma boa ideia que uma função só retorne mais de um tipo de dado:
+
+function terceiraIdade(idade) {
+    if (typeof idade !== 'number') {
+        return 'Informe sua idade'
+    } else if (idade >= 60) {
+        return true
+    } else {
+        return false
+    }
+    
+}
+
+console.log(terceiraIdade('oi'))
+
+function precisoVisitar(paisesVisitados) {
+    var totalPaises = 193
+    return `Ainda faltam ${totalPaises - paisesVisitados} países para você visitar`
+}
+
+console.log(totalPaises) //se executarmos essa linha não dá certo, porque não conseguimos ver uma variável que foi definida dentro de uma função, fora dela, pois foi definida localmente
+precisoVisitar(50)
