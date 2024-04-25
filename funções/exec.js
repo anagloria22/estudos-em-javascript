d8f7bd4125cba1629d7c801ff259dc1c28a40b77
Ana Glória <anaparente196@gmail.com>
2024-04-24 23:47:24 -0300

    Update exec.js

--- funções/exec.js
+++ funções/exec.js
@@ -26,3 +26,17 @@ function mostraMeuNome() {
     console.log('Glorinha')
 }
 addEventListener('scroll', mostraMeuNome())
+
+
+var totalPaises = 193
+function precisoVisitar(paisesVisitados) {
+    
+    return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`
+}
+
+function jaVisitei(paisesVisitados) {
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`
+}
+
+console.log(precisoVisitar(20))
+console.log(jaVisitei(20))
