228fb2927028a8cd0860d10167ab1bd829c51452
Ana Glória <anaparente196@gmail.com>
2024-04-27 14:49:37 -0300

    Update objeto.js

--- objetos/objeto.js
+++ objetos/objeto.js
@@ -5,17 +5,22 @@ var pessoa = {
     cidade: 'Sobral'
 }
 
-// étodo tem função em uma de suas propriedades
+// método tem função em uma de suas propriedades
 var quadrado = {
     lados: 4,
-    area (lado) {
+    area(lado) {
         return lado * lado
     },
-    perimetro (lado) {
-        return this.lados * lado //o this é o próprio objeto e faz com quenão precisemos repetir o nome. ele simplifica
+    perimetro(lado) {
+        return this.lados * lado //o this é o próprio objeto e faz com quenão precisemos repetir o nome dele simplifica
     },
 }
-console.log(quadrado.lados)
-console.log(area(5))
-console.log(perimetro(5))
+console.log(quadrado.area(5))
+console.log(quadrado.perimetro(5))
 //objetos são bons para organizar o código em pequenas partes reutilizáveis
+
+Math.PI // retorna 3,14 que é o valor de pi
+Math.random() // retorna um número aleatório 
+
+var pi = Math.PI //Math é um objeto nativo de javascript
+console.log(pi) //console é um objeto e log() é um método
