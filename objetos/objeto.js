1e4f7fb825db7ad957fb8aea54497d9eec1781ec
Ana Glória <anaparente196@gmail.com>
2024-04-28 18:04:21 -0300

    Update objeto.js

--- objetos/objeto.js
+++ objetos/objeto.js
@@ -25,3 +25,20 @@ Math.random() // retorna um número aleatório
 var pi = Math.PI //Math é um objeto nativo de javascript
 console.log(pi) //console é um objeto e log() é um método
 //se no console do navegador colocarmos console.table(quadrado) ele vai organizar numa tabela os dados dessa variável
+
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: '#ffffff'
+}
+
+var bg = menu.backgroundColor // vai retornar #ffffff
+//utilizamos o . para acessar as propriedades de um objeto(chamamos isso de Dot Notation Get). com ele também podemos adicionar novos métodos e propriedades por ex.:
+menu.color = 'blue'
+menu.esconder = function() {
+    console.log('Escondi')
+}
+
+menu.backgroundColor = '#000000'
+console.log(menu.backgroundColor) //retorna #000000
+//quando substituímos o valor de uma propriedade por outro chamamos de Dot Notation Set
