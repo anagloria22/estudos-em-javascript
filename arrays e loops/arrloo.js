76e7d23b6772dfbad9b9a4ad98c023ea60975f7c
Ana Glória <anaparente196@gmail.com>
2024-05-01 18:19:44 -0300

    create

---
+++ arrays e loops/arrloo.js
@@ -0,0 +1,10 @@
+var videoGames = ['Switch', 'PS4', ' Xbox']
+// videoGames.pop() elimina o ultimo elemento do array e mostra ele na tela
+
+var ultimoItem = videoGames.pop()
+
+videoGames.push('3DS') //adiciona no final
+
+for (var numero = 0; numero < 10; numero++) {
+    console.log(numero)
+}
